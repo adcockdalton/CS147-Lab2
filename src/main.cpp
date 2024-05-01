2c13edf2ea39633e974c05b7e48bfbfcda751ea2
Dalton Adcock <dadcock@uci.edu>
2024-04-30 22:19:46 -0700

    working final version

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,22 @@
 #define RED_YELLOW_STATE 1
 #define YELLOW_STATE 2
 #define GREEN_STATE 3
+#define WAIT_STATE 4
 #define BUZZER_ON_STATE 0
 #define BUZZER_OFF_STATE 1
+#define PRINT_MILLIS 100
 #define RED_MILLIS 10000
 #define RED_YELLOW_MILLIS 2000
 #define YELLOW_MILLIS 2000
 #define GREEN_MILLIS 5000
+#define WAIT_MILLIS 5000
 #define RED_BUZZER_ON_MILLIS 250
 #define RED_BUZZER_OFF_MILLIS 250
 #define GREEN_BUZZER_ON_MILLIS 500
 #define GREEN_BUZZER_OFF_MILLIS 1500
 int tl_state;
 int buzz_state;
+unsigned long print_timer;
 unsigned long tl_timer; // Traffic light timer.
 unsigned long buzz_timer;
 
@@ -47,6 +51,7 @@ void setup()
   // Initial state for states and timers..
   tl_state = RED_STATE;
   buzz_state = BUZZER_OFF_STATE;
+  print_timer = millis() + PRINT_MILLIS;
   tl_timer = millis() + RED_MILLIS;
   buzz_timer = millis() + RED_BUZZER_ON_MILLIS;
 }
@@ -56,25 +61,19 @@ void loop()
   switch (tl_state)
   {
   case RED_STATE:
-    if (millis() % 1000 == 0)
-      Serial.println("Red state...\n");
     digitalWrite(RED_PIN, HIGH);
     if (millis() > tl_timer)
     {
-      Serial.println("Switching from red to red-yellow...\n");
       digitalWrite(RED_PIN, LOW);
       tl_timer = millis() + RED_YELLOW_MILLIS;
       tl_state = RED_YELLOW_STATE;
     }
     break;
   case RED_YELLOW_STATE:
-    if (millis() % 1000 == 0)
-      Serial.println("Red-yellow state...\n");
     digitalWrite(RED_PIN, HIGH);
     digitalWrite(YELLOW_PIN, HIGH);
     if (millis() > tl_timer)
     {
-      Serial.println("Switching from red-yellow to green...\n");
       digitalWrite(RED_PIN, LOW);
       digitalWrite(YELLOW_PIN, LOW);
       tl_timer = millis() + GREEN_MILLIS;
@@ -82,22 +81,26 @@ void loop()
     }
     break;
   case YELLOW_STATE:
-    if (millis() % 1000 == 0)
-      Serial.println("Yellow state...\n");
-    if (tl_timer < millis())
+    digitalWrite(YELLOW_PIN, HIGH);
+    if (millis() > tl_timer)
     {
-      Serial.println("Switching from yellow to red...\n");
+      digitalWrite(YELLOW_PIN, LOW);
       tl_timer = millis() + RED_MILLIS;
       tl_state = RED_STATE;
     }
     break;
   case GREEN_STATE:
-    if (millis() % 1000 == 0)
-      Serial.println("Green state...\n");
     digitalWrite(GREEN_PIN, HIGH);
-    if (tl_timer < millis() && digitalRead(BUTTON_PIN) == HIGH)
+    if (millis() > tl_timer && digitalRead(BUTTON_PIN) == HIGH)
     {
-      Serial.println("Switching from green to yellow...\n");
+      tl_timer = millis() + WAIT_MILLIS;
+      tl_state = WAIT_STATE;
+    }
+    break;
+  case WAIT_STATE:
+    digitalWrite(GREEN_PIN, HIGH);
+    if (millis() > tl_timer)
+    {
       digitalWrite(GREEN_PIN, LOW);
       tl_timer = millis() + YELLOW_MILLIS;
       tl_state = YELLOW_STATE;
@@ -108,36 +111,35 @@ void loop()
   switch (buzz_state)
   {
   case BUZZER_ON_STATE:
-    if (millis() % 1000 == 0)
-      Serial.println("Buzzer on state...\n");
     if (tl_state == YELLOW_STATE || tl_state == RED_YELLOW_STATE)
     {
       unsetTone(BUZZER_PIN);
       buzz_state = BUZZER_OFF_STATE;
       buzz_timer = millis() + RED_BUZZER_OFF_MILLIS;
     }
-    else if (buzz_timer < millis())
+    else if (millis() > buzz_timer)
     {
-      Serial.println("Turning off buzzer...\n");
-      unsetTone(BUZZER_PIN);
       buzz_state = BUZZER_OFF_STATE;
-      if (tl_state == GREEN_STATE)
+      if (tl_state == GREEN_STATE || tl_state == WAIT_STATE)
+      {
+        unsetTone(BUZZER_PIN);
         buzz_timer = millis() + GREEN_BUZZER_OFF_MILLIS;
+      }
       else // (tl_state == RED_STATE)
+      {
+        unsetTone(BUZZER_PIN);
         buzz_timer = millis() + RED_BUZZER_OFF_MILLIS;
+      }
     }
     break;
   case BUZZER_OFF_STATE:
-    if (millis() % 1000 == 0)
-      Serial.println("Buzzer off state...\n");
     if (tl_state == YELLOW_STATE || tl_state == RED_YELLOW_STATE)
     {
       buzz_timer = millis() + RED_BUZZER_OFF_MILLIS;
     }
-    else if (buzz_timer < millis())
+    else if (millis() > buzz_timer)
     {
-      Serial.println("Turning on buzzer...\n");
-      if (tl_state == GREEN_STATE)
+      if (tl_state == GREEN_STATE || tl_state == WAIT_STATE)
       {
         buzz_state = BUZZER_ON_STATE;
         setTone(BUZZER_PIN);
@@ -151,5 +153,43 @@ void loop()
       }
     }
     break;
+  }
+
+  if (millis() > print_timer)
+  {
+    switch (tl_state)
+    {
+    case RED_STATE:
+      Serial.print("RED_STATE ");
+      break;
+    case RED_YELLOW_STATE:
+      Serial.print("RED_YELLOW_STATE ");
+      break;
+    case YELLOW_STATE:
+      Serial.print("YELLOW_STATE ");
+      break;
+    case GREEN_STATE:
+      Serial.print("GREEN_STATE ");
+      break;
+    }
+    switch (buzz_state)
+    {
+    case BUZZER_ON_STATE:
+      Serial.print("BUZZER_ON_STATE ");
+      break;
+    case BUZZER_OFF_STATE:
+      Serial.print("BUZZER_OFF_STATE ");
+      break;
+    }
+    switch (digitalRead(BUTTON_PIN))
+    {
+    case HIGH:
+      Serial.println("BUTTON_PIN_HIGH");
+      break;
+    case LOW:
+      Serial.println("BUTTON_PIN_LOW");
+      break;
+    }
+    print_timer = millis() + PRINT_MILLIS;
   }
 }
